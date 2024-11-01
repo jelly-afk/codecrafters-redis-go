b06b35d6a2b8484a46476f788207bc0ab03e5390
Chetan <jellybeans33124@gmail.com>
2024-11-01 12:04:20 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -15,28 +15,32 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	
-	 l, err := net.Listen("tcp", "0.0.0.0:6379")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
-	 }
+    l, err := net.Listen("tcp", "0.0.0.0:6379")
+    if err != nil {
+        fmt.Println("Failed to bind to port 6379")
+        os.Exit(1)
+    }
     tcpConn, err := l.Accept()
     defer tcpConn.Close()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
-    readBuffer := make([]byte, 14)
-    _, err = tcpConn.Read(readBuffer)
     if err != nil {
-        log.Fatal(err)
+        fmt.Println("Error accepting connection: ", err.Error())
+        os.Exit(1)
     }
-    fmt.Println(bytes.Compare(readBuffer, []byte("*1\r\n$4\r\nPING\r\n")))
-    _, err = tcpConn.Write([]byte("+PONG\r\n"))
-    if err != nil {
-        log.Fatal(err)
+    for {
+        readBuffer := make([]byte, 512)
+        _, err = tcpConn.Read(readBuffer)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(bytes.Compare(readBuffer, []byte("*1\r\n$4\r\nPING\r\n")))
+        _, err = tcpConn.Write([]byte("+PONG\r\n"))
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println("readBuffer: ", string(readBuffer))
+
     }
-    fmt.Println("readBuffer: ", string(readBuffer))
+
     
 
 }
